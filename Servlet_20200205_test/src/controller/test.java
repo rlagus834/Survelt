e6f3b9c69c17feb939694181705136d13f4b359e
6 <6@6-PC>
2020-02-05 13:35:03 +0900

    ㅁㅊㅁㄴ

--- Servlet_20200205_test/src/controller/test.java
+++ Servlet_20200205_test/src/controller/test.java
@@ -14,7 +14,7 @@ import service.InputService; //service 다른 패키지에 있어서 import
  */
 @WebServlet("/inputs")
 public class test extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;//노란warring안뜨게
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -22,9 +22,9 @@ public class test extends HttpServlet {
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
-	boolean result=true;
-		String var1=request.getParameter("na1");
-	InputService service=new InputService();
+	boolean result=true;//boolean 변수 하나만듬 (리턴받는것을 대입하기위해
+		String var1=request.getParameter("na1");//na1의 매개변수에 대입하기위해
+	InputService service=new InputService();//service의메소드를사용하기위해 대입
 	result=service.insert(var1);
 	if(result) {
 		response.sendRedirect("suuggong.jsp");
